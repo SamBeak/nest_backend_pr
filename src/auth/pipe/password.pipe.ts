26958af998ab116bc71d9eb7837b92fab70a86b6
SamBeak <baekhg102@korea.ac.kr>
2025-05-18 08:19:17 +0900

    feat: implement auth controller and password validation pipes

--- src/auth/pipe/password.pipe.ts
+++ src/auth/pipe/password.pipe.ts
@@ -3,10 +3,53 @@ import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from
 @Injectable()
 export class PasswordPipe implements PipeTransform {
 	transform(value: any, metadata: ArgumentMetadata) {
-		if (value.toString().length > 8) {
-			throw new BadRequestException("비밀번호는 8자 이하로 입력해주세요.");
+		if (typeof value !== "string") {
+			throw new BadRequestException("문자열이 아닙니다.");
 		}
 		
-		return value.toString();
+		if (metadata.type === "param") {
+			if (value.length > 8) {
+				throw new BadRequestException("비밀번호는 8자 이하로 입력해주세요.");
+			}
+		}
+		
+		return value;
+	}
+}
+
+@Injectable()
+export class MaxLengthPipe implements PipeTransform {
+	constructor(private readonly length: number) {}
+	transform(value: any, metadata: ArgumentMetadata) {
+		if (typeof value !== "string") {
+			throw new BadRequestException("문자열이 아닙니다.");
+		}
+		
+		if (metadata.type === "param") {
+			if (value.length > this.length) {
+				throw new BadRequestException(`최대 ${this.length}자 이하로 입력해주세요.`);
+			}
+		}
+		
+		return value;
+	}
+}
+
+@Injectable()
+export class MinLengthPipe implements PipeTransform {
+	constructor(private readonly length: number) {}
+	
+	transform(value: any, metadata: ArgumentMetadata) {
+		if (typeof value !== "string") {
+			throw new BadRequestException("문자열이 아닙니다.");
+		}
+		
+		if (metadata.type === "param") {
+			if (value.length < this.length) {
+				throw new BadRequestException(`최소 ${this.length}자 이상으로 입력해주세요.`);
+			}
+		}
+		
+		return value;
 	}
 }
