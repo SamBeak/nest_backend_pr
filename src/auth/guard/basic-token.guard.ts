dff7951a578bb3f684ee0d14cc02dd67af7bd2ff
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-05-20 08:59:37 +0900

    feat: enhance BasicTokenGuard to include token extraction and user authentication

--- src/auth/guard/basic-token.guard.ts
+++ src/auth/guard/basic-token.guard.ts
@@ -1,8 +1,35 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { AuthService } from "../auth.service";
 
+@Injectable()
 export class BasicTokenGuard implements CanActivate {
+	constructor(
+		private readonly authService: AuthService,
+	) {}
 	
 	async canActivate(context: ExecutionContext): Promise<boolean>{
-		const request = context.switchToHttp().getRequest();
+		const req = context.switchToHttp().getRequest();
+		
+		// {authorization : 'Basic asdsadasfasfqwqwrqwr'}
+		const rowToken = req.headers['authorization'];
+		
+		if (!rowToken) {
+			throw new UnauthorizedException('토큰이 없습니다');
+		}
+		
+		const token = this.authService.extractTokenFromHeader(rowToken, false);
+		
+		const {email, password} = this.authService.decodeBasicToken(token);
+		
+		const userInfo = {
+			email,
+			password,
+		};
+		
+		const user = await this.authService.authenticateWithEmailAndPassword(userInfo);
+		
+		req.user = user;
+		
+		return true;
 	}
 }
