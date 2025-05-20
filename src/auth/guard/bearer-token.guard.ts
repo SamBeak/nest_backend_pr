938b5e27fb17003a1d3a0135c5846f89e3f095d0
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-05-20 12:58:24 +0900

    feat: implement BaererTokenGuard and AccessTokenGuard for token validation and authorization

---
+++ src/auth/guard/bearer-token.guard.ts
@@ -0,0 +1,47 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../../users/users.service';
+import { AuthService } from '../auth.service';
+@Injectable()
+export class BaererTokenGuard implements CanActivate {
+	constructor(
+		private readonly authService: AuthService,
+		private readonly usersService: UsersService,
+	) {}
+	
+	async canActivate(context: ExecutionContext): Promise<boolean>{
+		const req = context.switchToHttp().getRequest();
+		
+		const rowToken = req.headers['authorization'];
+		
+		if (!rowToken) {
+			throw new UnauthorizedException('토큰이 없습니다');
+		}
+		
+		const token = this.authService.extractTokenFromHeader(rowToken, true);
+		
+		const result = await this.authService.verifyToken(token);
+		
+		const user = await this.usersService.getUserByEmail(result.email);
+		
+		req.token = token;
+		req.tokenType = result.type;
+		req.user = user;
+		
+		return true;
+	}
+}
+@Injectable()
+export class AccessTokenGuard extends BaererTokenGuard {
+	async canActivate(context: ExecutionContext): Promise<boolean>{
+		
+		await super.canActivate(context);
+		
+		const req = context.switchToHttp().getRequest();
+		
+		if (req.tokenType !== 'access') {
+			throw new UnauthorizedException('엑세스 토큰이 아닙니다');
+		}
+		
+		return true;
+	}
+}
