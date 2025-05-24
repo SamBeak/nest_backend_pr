7d22fc758887e649fac14c0001621ff0fc1f2967
SamBeak <baekhg102@korea.ac.kr>
2025-05-24 22:59:43 +0900

    feat: add User decorator for extracting authenticated user data from request context

--- src/users/decorator/users.decorator.ts
+++ src/users/decorator/users.decorator.ts
@@ -1,12 +1,17 @@
 import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
+import { UsersModel } from "../entities/users.entity";
 
-export const User = createParamDecorator((data, context: ExecutionContext) => {
+export const User = createParamDecorator((data: keyof UsersModel | undefined, context: ExecutionContext) => {
 	const req = context.switchToHttp().getRequest();
 	
-	const user = req.user;
+	const user = req.user as UsersModel;
 	
 	if (!user) {
 		throw new InternalServerErrorException("User decorator는 AccessTokenGuard와 함께 사용해야합니다. Request에 user 프로퍼티가 없습니다.");
+	}
+	
+	if(data) {
+		return user[data];
 	}
 	
 	return user;
