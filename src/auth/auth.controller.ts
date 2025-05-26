8977ec1ede28c329db78d903c3ce686bff59aa5f
SamBeak <baekhg102@korea.ac.kr>
2025-05-26 19:30:43 +0900

    feat: implement auth controller with email login, registration and token rotation endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MaxLengthPipe, MinLengthPipe } from './pipe/password.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -17,7 +18,7 @@ export class AuthController {
   registerEmail(
 	@Body('nickname') nickname: string,
 	@Body('email') email: string,
-	@Body('password') password: string,
+	@Body('password', new MaxLengthPipe(8), new MinLengthPipe(3)) password: string,
   ) {
 	return this.authService.registerWithEmail({ nickname, email, password });
   }
