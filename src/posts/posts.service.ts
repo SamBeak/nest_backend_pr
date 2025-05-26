10017512ff63ffb13a1bdd74ceaabe265bb00bfc
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-05-26 17:28:52 +0900

    feat: implement PostsService with CRUD operations for posts management

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PostsModel } from './entities/posts.entity';
 import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
 export class PostsService {
@@ -9,4 +10,37 @@ export class PostsService {
         @InjectRepository(PostsModel)
         private readonly postsRepository: Repository<PostsModel>,
     ){}
+    
+    async getAllPosts() {
+        return await this.postsRepository.find();
+    }
+    
+    async getPostById(id: number,) {
+        const post = await this.postsRepository.findOne({
+            where: {
+                id,
+            }
+        })
+        
+        if (!post) {
+            throw new NotFoundException();
+        }
+        
+        return post;
+    }
+    
+    async createPost(authorId: number, postDto: CreatePostDto,) {
+        const post = this.postsRepository.create({
+            // author: {
+            //     id: authorId,
+            // },
+            ...postDto,
+            // likeCount: 0,
+            // commentCount: 0,
+        });
+        
+        const newPost = await this.postsRepository.save(post);
+        
+        return newPost;
+    }
 }
