be3a6f357c4be253d49d9082d913abaac28275f0
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-05-27 16:36:47 +0900

    feat: create UsersModel entity with email, password and role fields

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -2,6 +2,8 @@ import { BaseModel } from "src/common/entities/base.entity";
 import { PostsModel } from "src/posts/entities/posts.entity";
 import { Column, Entity, OneToMany } from "typeorm";
 import { RolesEnum } from "../const/roles.const";
+import { IsString, Length } from "class-validator";
+import { Exclude } from "class-transformer";
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -16,6 +18,15 @@ export class UsersModel extends BaseModel {
 	email: string;
 	
 	@Column()
+	@IsString({
+		message: "비밀번호는 문자열이어야 합니다.",
+	})
+	@Length(3, 8, {
+		message: "비밀번호는 3자 이상 8자 이하로 입력해주세요.",
+	})
+	@Exclude({
+		toPlainOnly: true,
+	})
 	password: string;
 	
 	@Column({
