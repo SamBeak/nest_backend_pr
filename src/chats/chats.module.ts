f25fdcfdb9ea01cb8cb1f627d83ac2c1972982e9
SamBeak <114740158+SamBeak@users.noreply.github.com>
2025-06-24 15:45:38 +0900

    feat: implement chat module
     implement chat module and socket authentication guard for WebSocket connections

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -8,12 +8,16 @@ import { CommonModule } from 'src/common/common.module';
 import { ChatsMessagesService } from './messages/messages.service';
 import { MessagesModel } from './messages/entity/messages.entity';
 import { ChatsMessagesController } from './messages/messages.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    CommonModule,
+    AuthModule,
+    UsersModule,
     TypeOrmModule.forFeature([
       ChatsModel,
-      CommonModule,
       MessagesModel,
     ]),
   ],
